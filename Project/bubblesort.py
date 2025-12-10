5bf78cfcacf2cd5a5098e101ffd2362baa2286d8
Kunyanat <Kunyanat.n@kkumail.com>
2025-12-10 19:28:14 +0700

    Add input nad output

--- Project/bubblesort.py
+++ Project/bubblesort.py
@@ -6,3 +6,21 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
+def get_input_and_sort():
+    print("--- Bubble Sort Algorithm ---")
+    input_str = input("Please Input Number (with space) : ")
+    
+    try:
+        data_to_sort = list(map(int, input_str.split()))
+        
+    except ValueError:
+        print("Error: Input must be only numbers separated by spaces.")
+        return
+
+    print(f"\n Input: {data_to_sort}")
+    sorted_result = bubble_sort(data_to_sort)  
+    
+    print(f"Bubble Sort: {sorted_result}")
+
+if _name_ == "_main_":
+    get_input_and_sort()
